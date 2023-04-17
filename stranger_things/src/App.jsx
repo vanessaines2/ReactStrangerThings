01441ca8f66d81359a6af422f22481603f6e778b
Vanessaines2 <Vanessaines2@icloud.com>
2023-04-17 11:10:33 -0400

    some changes

--- stranger_things/src/App.jsx
+++ stranger_things/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      
+      <h1>Hello</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
