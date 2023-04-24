8e45ef7e4a940353900e4f49d773f36f1af86d7b
choilin310 <choiinhwan0@gmail.com>
2023-04-24 12:00:07 -0400

    css changes

--- stranger_things/components/AllPosts.jsx
+++ stranger_things/components/AllPosts.jsx
@@ -23,27 +23,27 @@ export default function AllPosts() {
 
   return (
     <div className="allPosts">
-      {data.length > 0 &&
-        data.map((data) => {
-          return (
-            <div key={data.author.id}>
-              <h1>username:{data.author.username}</h1>
-              <h2>Title :{data.title}</h2>
-              <p> {data.description}</p>
-              <h5>Price: {data.price}</h5>
-              <button
-                onClick={async (e) => {
-                  e.preventDefault();
-                  await deletePosts(token, data._id);
-                  console.log("ive been clicked");
-                }}
-              >
-                {" "}
-                Delete Post
-              </button>
-            </div>
-          );
-        })}
-    </div>
+  {data.length > 0 &&
+    data.map((data) => {
+      return (
+        <div className="post" key={data.author.id}>
+          <h1 className="post-username">Username: {data.author.username}</h1>
+          <h2 className="post-title">Title: {data.title}</h2>
+          <p className="post-description">{data.description}</p>
+          <h5 className="post-price">Price: {data.price}</h5>
+          <button
+            className="post-delete-button"
+            onClick={async (e) => {
+              e.preventDefault();
+              await deletePosts(token, data._id);
+              console.log("I've been clicked");
+            }}
+          >
+            Delete Post
+          </button>
+        </div>
+      );
+    })}
+</div>
   );
 }
